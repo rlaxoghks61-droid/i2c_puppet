c82937b20f56087b6ba62e3b6df93bdc57803cd6
rlaxoghks61-droid <rlaxoghks61@gmail.com>
2026-06-12 18:16:05 +0900

    29Update keyboard.c

--- app/keyboard.c
+++ app/keyboard.c
@@ -44,7 +44,7 @@ static const struct entry kbd_entries[][NUM_OF_COLS] =
 	{ { },                 { ' ', '\t' },             { 'C', '9' },              { 'Z', '7' },              { 'M', '.'  },  { 'N', ','  } },
 	{ { KEY_BTN_LEFT2 },   { .mod = KEY_MOD_ID_SYM }, { 'T', '(' },              { 'D', '5' },              { 'I', '-'  },  { 'Y', ')'  } },
 	{ { KEY_BTN_RIGHT1 },  { .mod = KEY_MOD_ID_ALT }, { 'V', '?' },              { 'X', '8' },              { '$', '`'  },  { 'B', '!'  } },
-	{ { },                 { 'A', '*' },              { .mod = KEY_MOD_ID_SHR }, { 'P', '@' },              { '\b' },       { '\n', '|' } },
+	{ { },                 { 'A', '*' },              { .mod = KEY_MOD_ID_SHR }, { 'P', '@' },              { '\b' },       { '\n', '\n' } },
 };
 
 #if NUM_OF_BTNS > 0
