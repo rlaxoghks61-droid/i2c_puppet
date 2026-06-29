86067427156720ac1a1397f613e8c01e6d21bfb3
rlaxoghks61-droid <rlaxoghks61@gmail.com>
2026-06-29 21:17:31 +0900

    Update esp_i2c.c

--- app/esp_i2c.c
+++ app/esp_i2c.c
@@ -2,25 +2,27 @@
 
 #define QUEUE_SIZE 32
 
-struct key_event_packet
+struct hid_event_packet
 {
-	uint8_t key;
+	uint8_t modifier;
+	uint8_t keycode;
 	uint8_t state;
 };
 
-static volatile struct key_event_packet queue[QUEUE_SIZE];
+static volatile struct hid_event_packet queue[QUEUE_SIZE];
 static volatile uint8_t head = 0;
 static volatile uint8_t tail = 0;
 
-void esp_i2c_push_key(char key, enum key_state state)
+void esp_i2c_push_hid(uint8_t modifier, uint8_t keycode, uint8_t state)
 {
 	uint8_t next = (head + 1) % QUEUE_SIZE;
 
 	if (next == tail)
 		return;
 
-	queue[head].key = (uint8_t)key;
-	queue[head].state = (uint8_t)state;
+	queue[head].modifier = modifier;
+	queue[head].keycode = keycode;
+	queue[head].state = state;
 
 	head = next;
 }
@@ -29,15 +31,18 @@ void esp_i2c_pop_key(uint8_t *buffer, uint8_t *len)
 {
 	if (tail != head)
 	{
-		buffer[0] = queue[tail].key;
-		buffer[1] = queue[tail].state;
+		buffer[0] = queue[tail].modifier;
+		buffer[1] = queue[tail].keycode;
+		buffer[2] = queue[tail].state;
+
 		tail = (tail + 1) % QUEUE_SIZE;
 	}
 	else
 	{
 		buffer[0] = 0;
 		buffer[1] = 0;
+		buffer[2] = 0;
 	}
 
-	*len = 2;
+	*len = 3;
 }
