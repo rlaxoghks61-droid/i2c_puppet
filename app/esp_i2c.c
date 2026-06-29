1386010e4da7595cae657de74a6b0bbcd2f7d5d9
rlaxoghks61-droid <rlaxoghks61@gmail.com>
2026-06-30 04:15:06 +0900

    Update esp_i2c.c

--- app/esp_i2c.c
+++ app/esp_i2c.c
@@ -17,43 +17,68 @@ static volatile uint8_t tail = 0;
 static void push_event(uint8_t type, uint8_t a, uint8_t b, uint8_t c)
 {
 	uint8_t next = (head + 1) % QUEUE_SIZE;
+
 	if (next == tail)
-		return;
+	{
+		tail = (tail + 1) % QUEUE_SIZE;
+	}
 
 	queue[head].type = type;
 	queue[head].a = a;
 	queue[head].b = b;
 	queue[head].c = c;
+
 	head = next;
 }
 
 void esp_i2c_push_hid(uint8_t modifier, uint8_t keycode, uint8_t state)
 {
-	push_event(ESP_EVT_KEY, modifier, keycode, state);
+	push_event(
+		ESP_EVT_KEY,
+		modifier,
+		keycode,
+		state
+	);
 }
 
 void esp_i2c_push_mouse(int8_t x, int8_t y, uint8_t buttons)
 {
-	push_event(ESP_EVT_MOUSE, (uint8_t)x, (uint8_t)y, buttons);
+	push_event(
+		ESP_EVT_MOUSE,
+		(uint8_t)x,
+		(uint8_t)y,
+		buttons
+	);
+}
+
+void esp_i2c_push_consumer(uint16_t usage, uint8_t state)
+{
+	push_event(
+		ESP_EVT_CONSUMER,
+		(uint8_t)(usage & 0xFF),
+		(uint8_t)((usage >> 8) & 0xFF),
+		state
+	);
 }
 
 void esp_i2c_pop_key(uint8_t *buffer, uint8_t *len)
 {
-	if (tail != head)
-	{
-		buffer[0] = queue[tail].type;
-		buffer[1] = queue[tail].a;
-		buffer[2] = queue[tail].b;
-		buffer[3] = queue[tail].c;
-		tail = (tail + 1) % QUEUE_SIZE;
-	}
-	else
+	if (tail == head)
 	{
 		buffer[0] = 0;
 		buffer[1] = 0;
 		buffer[2] = 0;
 		buffer[3] = 0;
+		*len = 4;
+		return;
 	}
+
+	buffer[0] = queue[tail].type;
+	buffer[1] = queue[tail].a;
+	buffer[2] = queue[tail].b;
+	buffer[3] = queue[tail].c;
+
+	tail = (tail + 1) % QUEUE_SIZE;
 
 	*len = 4;
 }
