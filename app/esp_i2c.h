12644c17f4f334e1aa81197a566085cce17510bf
rlaxoghks61-droid <rlaxoghks61@gmail.com>
2026-06-29 21:16:42 +0900

    Update esp_i2c.h

--- app/esp_i2c.h
+++ app/esp_i2c.h
@@ -1,9 +1,8 @@
 #pragma once
 
 #include <stdint.h>
-#include "keyboard.h"
 
 #define REG_ID_ESP_KEY_EVENT 0x70
 
-void esp_i2c_push_key(char key, enum key_state state);
+void esp_i2c_push_hid(uint8_t modifier, uint8_t keycode, uint8_t state);
 void esp_i2c_pop_key(uint8_t *buffer, uint8_t *len);
