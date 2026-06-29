0bb467fa2040cbea71a6ed72b5725295a5587db9
rlaxoghks61-droid <rlaxoghks61@gmail.com>
2026-06-29 22:54:31 +0900

    Update esp_i2c.h

--- app/esp_i2c.h
+++ app/esp_i2c.h
@@ -4,5 +4,9 @@
 
 #define REG_ID_ESP_KEY_EVENT 0x70
 
+#define ESP_EVT_KEY   1
+#define ESP_EVT_MOUSE 2
+
 void esp_i2c_push_hid(uint8_t modifier, uint8_t keycode, uint8_t state);
+void esp_i2c_push_mouse(int8_t x, int8_t y, uint8_t buttons);
 void esp_i2c_pop_key(uint8_t *buffer, uint8_t *len);
