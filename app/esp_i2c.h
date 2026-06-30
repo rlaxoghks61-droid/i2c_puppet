18aaccc9500eae05163d8030395bb79b19d15be6
rlaxoghks61-droid <rlaxoghks61@gmail.com>
2026-06-30 11:02:21 +0900

    Update esp_i2c.h

--- app/esp_i2c.h
+++ app/esp_i2c.h
@@ -8,6 +8,8 @@
 #define ESP_EVT_MOUSE    2
 #define ESP_EVT_CONSUMER 3
 
+void esp_i2c_init(void);
+
 void esp_i2c_push_hid(uint8_t modifier, uint8_t keycode, uint8_t state);
 void esp_i2c_push_mouse(int8_t x, int8_t y, uint8_t buttons);
 void esp_i2c_push_consumer(uint16_t usage, uint8_t state);
